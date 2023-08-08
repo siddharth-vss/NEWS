1dfda2e1a6269308b68d1dad83491868683df76d
Siddharth <siddharth@vasantsoftwaressolution.com>
2023-08-08 19:00:42 +0530

    redy to deploy

--- src/components/newsitems.js
+++ src/components/newsitems.js
@@ -14,7 +14,7 @@ const Newsitems = (props)=>  {
                 }> 
                     <span className={`badge rounded-pill bg-${mode}`}> {source} </span>
                 </div>
-            <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} style={{width:"325px"}} alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}..</h5>
                     <p className="card-text">{description}..</p>
